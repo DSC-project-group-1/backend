bdc62a5be728537290f16da70807c27b26653b51
Namokar100 <namokarsvg@gmail.com>
2024-12-17 21:00:49 +0530

    User model, authentication and storage of user games added

--- server.js
+++ server.js
@@ -8,14 +8,20 @@ const analyzeRoutes = require('./routes/analyzeRoutes');
 const resultRoutes = require('./routes/resultRoutes');
 const { initSocket } = require('./sockets/gameSocket');
 const errorHandler = require('./utils/errorHandler');
+const gameRoutes = require('./routes/gameRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/audio', audioRoutes);
 app.use('/api/analyze', analyzeRoutes);
 app.use('/api/result', resultRoutes);
+app.use('/api/game', gameRoutes);
+app.use('/api/auth', authRoutes);
 
 // Socket.io setup
 const server = app.listen(PORT, () => {
